ab7c893a0d4ebb3f1726501765a9590409ed61a2
Andre <andre@labtime.ufg.br>
2019-11-04 14:41:15 -0300

    Router scroll position restoration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
-import {filter} from 'rxjs/operators';
-import {Subscription} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  subscription: Subscription;
-
-  constructor(private router: Router) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.subscription = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    )
-      .subscribe(() => window.scrollTo(0, 0));
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
